0c1375ac0a93967304c7c3b3544bdf9c37cb0fb7
Tachy-22 <entekumejeffrey@gmail.com>
2023-12-27 12:48:35 +0100

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const footerLinks = [
   {
     title: "Quicklinks",
-    links:  [
+    links: [
       { social: "Support", link: "/" },
       { social: "Contact", link: "/" },
     ],
@@ -22,7 +22,7 @@ const footerLinks = [
 const Footer = () => {
   return (
     <footer>
-      <div className="flex md:flex-row flex-col justify-between items-center w-full mx-auto max-w-[85%] p-[3rem]  ">
+      <div className="flex md:flex-row flex-col justify-between items-start w-full mx-auto max-w-[85%] md:p-[3rem]  ">
         <div className="flex flex-col gap-[3rem] ">
           <Logo size={`2xl`} />
           <span className="flex   rounded-[5rem] transition-colors duration-500 border border-blue-400 hover:bg-blue-300/30 px-4 py-2 gap-3">
@@ -31,9 +31,9 @@ const Footer = () => {
           </span>
         </div>
 
-        <div className="grid grid-cols-2 gap-6 pt-[3rem] md:pt-0">
+        <div className="flex items-start justify-between md:justify-end gap-6 pt-[3rem] md:pt-0 w-full brder">
           {footerLinks.map((group, index) => (
-            <div key={index} className="flex flex-col">
+            <div key={index} className="flex flex-col w-fit ">
               <small className="pb-4 text-gray-300">{group.title}</small>
               {group.links.map(({ link, social }, idx) => (
                 <Link
